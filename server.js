7656bfa7d625dee9153ad62df81a04ed468da3de
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-05-31 12:02:35 -0600

    added server file, listening on port 8080, connected as id 71

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+// Server as static content for the app from the "public" directory
+app.use(express.static("public"));
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars
+var exprhbs = require("express-handlebars");
+app.engine("handlebars", exprhbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Start the server so taht it can begin listening to client requests
+app.listen(PORT, function() {
+    console.log("Server listening on: " + PORT);
+});
+
