b409917444f569ec4d6764db53164e836dfdc028
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-06-02 04:04:39 -0600

    Devour buttons are not working when pressed. Checked orm connection and it is working

--- config/orm.js
+++ config/orm.js
@@ -1,10 +1,3 @@
-// In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
-
-//      * `selectAll()`
-//      * `insertOne()`
-//      * `updateOne()`
-
-
 // Import MySQL connection to create a connection to the entire application
 var connection = require("../config/connection.js");
 
