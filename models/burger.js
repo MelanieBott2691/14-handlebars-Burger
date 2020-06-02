f226a836a97e1a23123e8f42dd732ff7d145855a
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-06-02 04:35:48 -0600

    deleted objColVals from the create and delete sections in the burger.js

--- models/burger.js
+++ models/burger.js
@@ -18,8 +18,8 @@ var burgers = {
       cb(res);
     });
   },
-  delete: function(objColVals, condition, cb) {
-    orm.delete("burgers", objColVals, condition, function(res) {
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
       cb(res);
     });
   }
