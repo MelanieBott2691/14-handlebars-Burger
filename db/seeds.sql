bda4dbf8d3c50bcfd40e43499a9a2bbf716b9923
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-06-01 23:47:00 -0600

    index.handlbars set up to hold buttons and input section to be formatted later

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 USE burgers_db;
 INSERT INTO burgers(burger_name, devoured) 
-VALUES('Hawaiian BBQ Burger', false), ('Hawaiian BBQ Burger', false), ('Little Bacon Cheeseburger', false), 
+VALUES('Hawaiian BBQ Burger', false), ('Little Bacon Cheeseburger', false), 
 ('Scrum-Delicious Burger', false), ('Chile Veggie Burger', false);
