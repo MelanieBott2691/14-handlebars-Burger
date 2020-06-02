4ed98d1d0000c3293d7e890f3b62716462ace3c7
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-06-02 00:27:37 -0600

    error still showing selectAll not a function, needed to change burgers controller and burgers js

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,10 @@
+// In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+
+//      * `selectAll()`
+//      * `insertOne()`
+//      * `updateOne()`
+
+
 // Import MySQL connection to create a connection to the entire application
 var connection = require("../config/connection.js");
 
@@ -70,23 +77,6 @@ var orm = {
     });
   }
 };
-// =================
-/*HOMEWORK INSTRUCTIONS
-In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
 
-     * `selectAll()`
-     * `insertOne()`
-     * `updateOne()`
-
-   * Export the ORM object in `module.exports`.*/
-
-//    ================
-
-// Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
-// In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-// ["?", "?", "?"].toString() => "?,?,?";
-
-// Export the orm object for the model (cat.js).
+// Export the orm object
 module.exports = orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,7 +8,7 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.selectAll(function(data) {
+  burger.all(function(data) {
     var hdbrsObject = {
       burgers: data
     };
@@ -17,7 +17,7 @@ router.get("/", function(req, res) {
   });
 
 router.post("/api/burgers", function(req, res) {
-    burger.insertOne([
+    burger.create([
     "burger_name", "devoured"
   ], [
     req.body.burger_name, req.body.devoured
@@ -33,7 +33,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-    burgers.updateOne({
+    burgers.update({
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
@@ -44,12 +44,12 @@ router.put("/api/burgers/:id", function(req, res) {
     }
   });
 });
-  router.deleteOne(condition, function(req, res) {
+  router.delete(condition, function(req, res) {
     var condition = "id = " + req.params.id;
   
     console.log("condition", condition);
   
-      burgers.deleteOne(condition, function(result){
+      burgers.delete(condition, function(result){
       if ((result, changedRows == 0)) {
         return res.status(404).end();
       } else {
