98649babdd23096d2d946534e670dd068005b9c6
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-06-01 23:00:14 -0600

    burgers.js set to create functions that will interact with the database

--- models/burger.js
+++ models/burger.js
@@ -2,20 +2,25 @@
 var orm = require("../config/orm.js");
 
 var burgers = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
     throw err;
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
+  insertOne: function(cols, vals, cb) {
+    orm.selectAll("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
+  updateOne: function(objColVals, condition, cb) {
+    orm.selectAll("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  deleteOne: function(objColVals, condition, cb) {
+    orm.selectAll("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   }
