fccfe9ba13f049c80b6b294876403435cc700332
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-06-01 22:56:13 -0600

    orm.js created with selectAll, insertOne, and updateOne

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,76 @@
-// Import MySQL connection.
+// Import MySQL connection to create a connection to the entire application
 var connection = require("../config/connection.js");
 
+// create question marks function
+function createQmarks(num) {
+  var arr = [];
+  for(var i = 0; i < num; i++){
+    arr.push("?");
+  }
+  return arr.toString();
+}
+  //turn the string into a readable query
+function translateSql(obj) {
+  var arr = [];
+  for(var key in ob){
+    var value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if(typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // push the array
+      arr.push(key + "=" + value)
+    }
+  }
+  return arr.toString();
+}
+// Create a variable called ORM and export it with all selections and queries
+var orm = {
+  selectAll: function(table, cb){
+    var dbQuery = "SELECT * FROM " + table + ";";
 
+    connection.query(dbQuery, function(err, res) {
+      if(err) {
+        throw err;
+      }
+      // callback
+      cb(res);
+    });
+  },
+  insertOne: function(table, cols, vals, cb) {
+    var dbQuery = "INSERT INTO " + table + " (" + cols.toString() + ") " + "VALUES (" + createQmarks(vals.length) + ") ";
+  
+    console.log(dbQuery);
+    connection.query(dbQuery, vals, function(err, res) {
+      if (err) {
+        throw err;
+      }
+      cb(res);
+    });
+  },
+  updateOne: function(table, objColVals, condition, cb) {
+    var dbQuery= "UPDATE " + table + " SET " + translateSql(objColVals) + " WHERE " + condition;
+
+    console.log(dbQuery);
+    connection.query(dbQuery, function(err, res) {
+      if (err) {
+        throw err;
+      }
+      cb(res);
+    });
+  },
+  deleteOne: function(table, condition, cb) {
+    var dbQuery = "DELETE FROM " + table + "WHERE " + condition;
+    
+    console.log(dbQuery);
+    connection.query(dbQuery, function(err, res) {
+      if (err) {
+        throw err;
+      }
+      cb(res);
+    });
+  }
+};
 // =================
 /*HOMEWORK INSTRUCTIONS
 In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
