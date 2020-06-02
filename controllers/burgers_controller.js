740aa3d3134eaa01b2b13ba7981867cec7248554
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-06-02 00:31:29 -0600

    reference error saying condition is not defined in burgers controller file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,11 +9,11 @@ var burger = require("../models/burger.js");
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.all(function(data) {
-    var hdbrsObject = {
+    var hbsObject = {
       burgers: data
     };
-    console.log(hdbrsObject);
-    res.render("index", hdbrsObject);
+    console.log(hbsObject);
+    res.render("index", hbsObject);
   });
 
 router.post("/api/burgers", function(req, res) {
